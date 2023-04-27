12cdfc0c95076e52d41f8b51ddbfe1c966f8d6eb
Huba <81635973+Wahabbbb@users.noreply.github.com>
2023-04-27 15:40:16 +0300

    Add files via upload

---
+++ inis_lab5/input.js
@@ -0,0 +1,50 @@
+const elements = Array.from(document.getElementsByClassName('target'))
+
+
+
+elements.forEach(element => {
+    element.addEventListener('mousedown', function handleClick(event){
+
+        function moveAt(pageX, pageY){
+            element.style.left = pageX - element.offsetWidth / 2 + 'px'
+            element.style.top = pageY - element.offsetHeight /2 + 'px'
+        }
+        
+        function onMouseMove(event){
+            moveAt(event.pageX, event.pageY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove)
+
+        document.addEventListener('click', function(){
+            document.removeEventListener('mousemove', onMouseMove)
+        })
+
+        document.addEventListener('keydown', function handleEscDown(event){
+            if (event.code == 'Escape'){
+                document.removeEventListener('mousemove', onMouseMove)
+            }
+        })
+    })
+
+    element.addEventListener('dblclick', function handleDbClick(event){
+
+        function moveAt(pageX, pageY){
+            element.style.left = pageX - element.offsetWidth / 2 + 'px'
+            element.style.top = pageY - element.offsetHeight /2 + 'px'
+        }
+        
+        function onMouseMove(event){
+            moveAt(event.pageX, event.pageY);
+        }
+        
+        document.addEventListener('mousemove', onMouseMove)
+
+        document.addEventListener('keydown', function handleEscDown(event){
+            if (event.code == 'Escape'){
+                document.removeEventListener('mousemove', onMouseMove)
+            }
+        })
+    })
+})
+
